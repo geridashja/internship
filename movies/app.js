e0e7973d7d3726dea5c287c0cbeb41a6905eba7f
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-01-29 18:25:49 +0300

    searchbar fix

--- movies/app.js
+++ movies/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Route = require('./routes/routes');
 const ejs = require('ejs');
+const bodyParser = require('body-parser')
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 app.set('view engine', 'ejs');
 const URI ="mongodb+srv://pokaripo:Geri001.@node.xyq4k.mongodb.net/movies?retryWrites=true&w=majority";
 
--- movies/routes/routes.js
+++ movies/routes/routes.js
@@ -1,3 +1,4 @@
+const { render } = require('ejs');
 const express = require('express');
 const path = require('path');
 const Movie = require('../module/model');
@@ -5,48 +6,35 @@ route = express.Router();
 
 //ROUTES
 route.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, '../views/home.html'));
-    //using ejs
     res.render('home');
 });
 
 route.get('/register_movies', (req, res) => {
-    // res.sendFile(path.join(__dirname, '../views/register_movies.html'));
-    //using ejs
     res.render('register_movies');
 });
 
 route.get('/show_movies', (req, res) => {
-    // res.sendFile(path.join(__dirname, '../views/show_movies.html'));
-    //using ejs
     res.render('show_movies');
 });
 
-// //get all registered users from database and show them to all_users view
-// route.get('/all_users', (req,res) => {
-//     Credentials.find((err, data)=>{
-//         if(err){
-//             console.log(err)
-//         }
-//         else{
-//             res.render('all_users', {users:data});
-//         }
-//     })
-// });
+//show books
+route.get('/show', (req,res) => {
+    let titlee = req.query;
+    Movie.findOne({title: titlee.title}).then((result) =>{
+        res.render('movie',{ title: result.title, year : result.year, isit: true});
+    }).catch((err)=> console.log(err));
+    console.log(titlee);
+});
 
 //register users
 route.post('/add', (req, res) => {
     const newmovie = new Movie(req.body);
     newmovie.save().then((result)=>{
-        // res.sendFile(path.join(__dirname, '../views/home.html'));
-        //using ejs
         res.render('home');
     }).catch((err) => console.log(err));
 });
 
-//delete users
 route.use((req,res)=>{
-    //using ejs
     res.render('404');
 });
 
