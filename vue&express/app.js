df300753175ee9758da331cf91a68748d8b9a988
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-01-29 16:05:36 +0300

    new

--- vue&express/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const approutes = require('./routes/approutes');
-const app = express();
-
-const URI = "mongodb+srv://pokaripo:Geri001.@node.xyq4k.mongodb.net/credentials?retryWrites=true&w=majority";
-mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true }).then((result)=> app.listen(3000)).catch((err)=>console.log(err));
-
-app.use(approutes);
