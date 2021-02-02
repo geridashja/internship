ecfd3dce17db8fd789c5bf0e3144e21a5190f7b1
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-03 01:01:54 +0300

    vue

---
+++ instat_data/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> | 
+    <router-link to="/show_data">Search for data</router-link>
+  </div>
+  <show-data />
+  <router-view/>
+</template>
+
+<script>
+import show_data from './views/show_data.vue'
+export default {
+  name: "App",
+  components: {
+    show_data
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ instat_data/src/views/show_data.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="show">
+    <h1>Show Data</h1>
+    <form>
+      <input type="text" v-model="year" placeholder="Enter the year">
+      <!-- <button type="submit">Get Data</button> -->
+    </form>
+    <p><strong>Year is:</strong> {{ year }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'show_data',
+  components: {
+    data: function (){
+      return {
+        year: ""
+      }
+    }
+  }
+}
+</script>
