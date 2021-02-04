a7ebd18c957440e759894806565e6aacee753726
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-04 15:03:15 +0300

    fixing bugs

---
+++ todo-vue/client/vue.config.js
@@ -0,0 +1,13 @@
+//this snippet was taken online 
+// it is used to make API calls from server to client and vice-versa 
+module.exports = {
+    configureWebpack: {
+        devServer: {
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:3000',
+                },
+            },
+        },
+    },
+}
