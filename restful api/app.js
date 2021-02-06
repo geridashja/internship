c7edd5e5714bd77b0d9c0010361a96b6373de54b
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-06 20:44:36 +0300

    creating server

---
+++ restful api/app.js
@@ -0,0 +1,8 @@
+const Express = require('express');
+const ejs = require('ejs');
+const router = require('./router/router');
+
+const app = Express();
+app.set('view engine', 'ejs');
+app.listen(3000);
+app.use(router);
