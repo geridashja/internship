d380b4cd876d2a7e1b1242e48833cc61bf80231b
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-07 16:36:38 +0300

    creating server

---
+++ recipes/app.js
@@ -0,0 +1,16 @@
+const Express = require('express');
+
+const router = require('./router/router');
+const { urlencoded } = require('express');
+
+const app = Express();
+
+//needed in order to get the data from forms
+app.use(urlencoded({extended:true}));
+
+app.use(Express.json());
+app.set('view engine', 'ejs');
+
+app.listen(3000);
+
+app.use(router);
---
+++ recipes/router/router.js
@@ -0,0 +1,52 @@
+const Express = require('express');
+
+const pool = require('../database');
+const router = Express.Router();
+
+//create a todo item
+router.post('/create', async(req,res) =>{
+    try {
+        const newitem = await pool.query("INSERT INTO all_recipes (body) VALUES ($1)", ["shto vaj"]);
+        // res.json(newitem);
+    } catch (err) {
+        console.log(err.message);
+    }
+    res.redirect('/');
+});
+
+router.get('/', async(req,res) =>{
+    try {
+        const todoitems = await pool.query('SELECT * FROM all_recipes');
+        // res.render('home', {todoitems: todoitems});
+        res.send(todoitems);
+        // console.log(todoitems);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+//delete items
+router.delete('/:id', async(req,res) => {
+    try {
+        const deleteditem = await pool.query('DELETE FROM all_recipes WHERE all_id = $1', [1]);
+    } catch (error) {
+        console.log(error.message);
+    }
+    res.redirect('/');
+});
+
+// //update items
+// router.post('/:id', async(req,res) =>{
+//     try {
+//         const id = req.params.id;
+//         const newbody = req.body.body;
+//         const updateditem = await pool.query("UPDATE todo SET body = $1 WHERE todo_id = $2", [newbody, id]);
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+//     res.redirect('/');
+// })
+
+
+
+module.exports = router;
