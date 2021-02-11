7378d5bd85a0bb5b21cd15fd7030d78d08c5d2b8
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-11 23:05:38 +0300

    csv saving

--- scraping booking/booking.js
+++ scraping booking/booking.js
@@ -1,5 +1,9 @@
 const puppeteer = require('puppeteer');
-let bookingUrl = 'https://www.booking.com/searchresults.html?aid=304142&label=gen173nr-1FCAEoggI46AdIM1gEaAaIAQGYATG4ARfIAQzYAQHoAQH4AQKIAgGoAgO4Ao-nlIEGwAIB0gIkYzQwY2I3ZmUtN2ExMi00NzIxLTg1MmMtYjc4YTRmYWY1NWYx2AIF4AIB&sid=0671f6e18fe455afd933f1aaccfd3eef&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.html%3Faid%3D304142%3Blabel%3Dgen173nr-1FCAEoggI46AdIM1gEaAaIAQGYATG4ARfIAQzYAQHoAQH4AQKIAgGoAgO4Ao-nlIEGwAIB0gIkYzQwY2I3ZmUtN2ExMi00NzIxLTg1MmMtYjc4YTRmYWY1NWYx2AIF4AIB%3Bsid%3D0671f6e18fe455afd933f1aaccfd3eef%3Bsb_price_type%3Dtotal%3Bsrpvid%3Db140719a2f8300f0%26%3B&ss=Turkey&is_ski_area=&checkin_year=&checkin_month=&checkout_year=&checkout_month=&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&ss_raw=Turkey&ac_position=0&ac_langcode=en&ac_click_type=b&dest_id=215&dest_type=country&place_id_lat=38.9637&place_id_lon=34&search_pageview_id=8957735abdd500a3&search_selected=true&search_pageview_id=8957735abdd500a3&ac_suggestion_list_length=5&ac_suggestion_theme_list_length=0';
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+
+
+let bookingUrl = 'https://www.booking.com/searchresults.html?label=gen173nr-1DCAEoggI46AdIM1gEaAaIAQGYATG4ARfIAQzYAQPoAQH4AQKIAgGoAgO4AurwlYEGwAIB0gIkNzExOWJkYTQtOGQ5ZS00MjViLTlhMjctNmJhNDUzOTE1M2Zj2AIE4AIB&sid=0671f6e18fe455afd933f1aaccfd3eef&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.html%3Flabel%3Dgen173nr-1DCAEoggI46AdIM1gEaAaIAQGYATG4ARfIAQzYAQPoAQH4AQKIAgGoAgO4AurwlYEGwAIB0gIkNzExOWJkYTQtOGQ5ZS00MjViLTlhMjctNmJhNDUzOTE1M2Zj2AIE4AIB%3Bsid%3D0671f6e18fe455afd933f1aaccfd3eef%3Bsb_price_type%3Dtotal%3Bsrpvid%3D79138278832d020f%26%3B&ss=Turkey&is_ski_area=0&checkin_year=&checkin_month=&checkout_year=&checkout_month=&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&ss_raw=Turkey&ac_position=0&ac_langcode=en&ac_click_type=b&dest_id=215&dest_type=country&place_id_lat=38.9637&place_id_lon=34&search_pageview_id=b46d8c370cc305f6&search_selected=true';
 
 
 async function getdata(page){
@@ -25,15 +29,33 @@ async function getdata(page){
           page.click("#search_results_table > div.bui-pagination.results-paging_simplified.js-results-paging > nav > ul > li.bui-pagination__item.bui-pagination__next-arrow > a");
           await page.waitFor(2000);
           results = results.concat(await getdata(page));
-        //   let new_res = await getdata(page);
-        //   results.push(new_res);
       }
       catch(error){
         console.log(error);
       }
     
     }
-    console.log(results);
-    console.log(results.length);
+
+    //converting results to csv file
+    const csvWriter = createCsvWriter({
+      path: 'file.csv',
+      header: [
+          {id: 'name', title: 'NAME'},
+          {id: 'city', title: 'CITY'},
+          {id: 'country', title: 'COUNTRY'}
+      ]
+    });
+    let res = [];
+    results.forEach(element => {
+      let obj = new Object();
+      obj.name = element.split(',')[0];
+      obj.city = element.split(',')[1];
+      obj.country = element.split(',')[2];
+      res.push(obj);
+    });
+    csvWriter.writeRecords(res)
+    .then(() => {
+      console.log('...Done');
+    });
     await browser.close();
 })();
