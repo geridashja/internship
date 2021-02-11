b1b0a5406b0fb4abaf8f0af8a4aceb1309671aec
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-11 16:04:49 +0300

    pagination fixed

--- scraping booking/app.js
+++ scraping booking/app.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let bookingUrl = 'https://www.booking.com/searchresults.html?label=gen173nr-1FCAEoggI46AdIM1gEaAaIAQGYATG4ARfIAQzYAQHoAQH4AQKIAgGoAgO4Ao-nlIEGwAIB0gIkYzQwY2I3ZmUtN2ExMi00NzIxLTg1MmMtYjc4YTRmYWY1NWYx2AIF4AIB&sid=0671f6e18fe455afd933f1aaccfd3eef&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.html%3Flabel%3Dgen173nr-1FCAEoggI46AdIM1gEaAaIAQGYATG4ARfIAQzYAQHoAQH4AQKIAgGoAgO4Ao-nlIEGwAIB0gIkYzQwY2I3ZmUtN2ExMi00NzIxLTg1MmMtYjc4YTRmYWY1NWYx2AIF4AIB%3Bsid%3D0671f6e18fe455afd933f1aaccfd3eef%3Bsb_price_type%3Dtotal%3Bsrpvid%3Dc70051e798dc007c%26%3B&ss=Turkey&is_ski_area=0&checkin_year=&checkin_month=&checkout_year=&checkout_month=&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&ac_position=0&ac_langcode=en&ac_click_type=b&dest_id=215&dest_type=country&place_id_lat=38.9637&place_id_lon=34&search_pageview_id=340951e944730135&search_selected=true&ss_raw=Turkey';
 (async () => {
     await self.start(bookingUrl);
-    let result = await self.result(500);
+    let result = await self.result();
     console.dir(result);
     console.dir(result.length);
     const header = ['hotel name'];
--- scraping booking/index.js
+++ scraping booking/index.js
@@ -11,13 +11,35 @@ const self = {
         await self.page.goto(url);
     },
 
-    result: async (nr) => {
+    result: async () => {
         let results = [];
 
-        do {
-            let new_result = await self.getdata();
-            results = [...results, ...new_result];
-            if(results.length < nr){
+        // do {
+        //     let new_result = await self.getdata();
+        //     results = [...results, ...new_result];
+        //     if(results.length < nr){
+        //         let nextPageButton = await self.page.$("#search_results_table > div.bui-pagination.results-paging_simplified.js-results-paging > nav > ul > li.bui-pagination__item.bui-pagination__next-arrow > a");
+        //         if(nextPageButton){
+        //             try{
+        //                 await nextPageButton.click();
+        //                 console.log("clicked");
+        //             }
+        //             catch(err){
+        //                 console.log(err);
+        //             }
+                    
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        // }while(results.length <=nr);
+        let lastpage = 40;
+        for (let index = 1; index < lastpage-1; index++) {
+            await self.page.waitFor(1000);
+            // let new_result = await self.getdata(self.page);
+            results = results.concat(await self.getdata(self.page));
+            if (index != lastpage - 1) {
                 let nextPageButton = await self.page.$("#search_results_table > div.bui-pagination.results-paging_simplified.js-results-paging > nav > ul > li.bui-pagination__item.bui-pagination__next-arrow > a");
                 if(nextPageButton){
                     try{
@@ -27,14 +49,14 @@ const self = {
                     catch(err){
                         console.log(err);
                     }
-                    
+                                
                 }
                 else{
-                    break;
+                     break;
                 }
             }
-        }while(results.length <=nr);
-        return results.slice(0,nr);
+        } 
+        return results;
     },
 
     getdata: async () => {
