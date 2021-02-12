9f8476127de510b0b4d9ab00140c871362c9839b
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-12 14:46:39 +0300

    calling scraped inside app

--- scraping booking/app.js
+++ scraping booking/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
-const db = require('./database');
+const pool = require('./database');
+const scrap = require('./booking');
+
 const app = express();
 
 
-app.get('/',(req,res) => {
-    res.sendFile('home.html', { root: path.join(__dirname) });
+app.get('/',async(req,res) => {
+    try {
+        await scrap();
+        res.sendFile('home.html', { root: path.join(__dirname) });
+        
+    } catch (error) {
+        console.log(error.message);
+    }
 })
+
 
 app.listen(3000);
--- scraping booking/booking.js
+++ scraping booking/booking.js
@@ -3,7 +3,7 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 
 
-let bookingUrl = 'https://www.booking.com/searchresults.html?label=gen173nr-1DCAEoggI46AdIM1gEaAaIAQGYATG4ARfIAQzYAQPoAQH4AQKIAgGoAgO4AurwlYEGwAIB0gIkNzExOWJkYTQtOGQ5ZS00MjViLTlhMjctNmJhNDUzOTE1M2Zj2AIE4AIB&sid=0671f6e18fe455afd933f1aaccfd3eef&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.html%3Flabel%3Dgen173nr-1DCAEoggI46AdIM1gEaAaIAQGYATG4ARfIAQzYAQPoAQH4AQKIAgGoAgO4AurwlYEGwAIB0gIkNzExOWJkYTQtOGQ5ZS00MjViLTlhMjctNmJhNDUzOTE1M2Zj2AIE4AIB%3Bsid%3D0671f6e18fe455afd933f1aaccfd3eef%3Bsb_price_type%3Dtotal%3Bsrpvid%3D79138278832d020f%26%3B&ss=Turkey&is_ski_area=0&checkin_year=&checkin_month=&checkout_year=&checkout_month=&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&ss_raw=Turkey&ac_position=0&ac_langcode=en&ac_click_type=b&dest_id=215&dest_type=country&place_id_lat=38.9637&place_id_lon=34&search_pageview_id=b46d8c370cc305f6&search_selected=true';
+let bookingUrl = 'https://www.booking.com/searchresults.html?label=gen173nr-1FCAEoggI46AdIM1gEaAaIAQGYATG4ARfIAQzYAQHoAQH4AQKIAgGoAgO4AvzJmYEGwAIB0gIkZDQ0YzIzZTItYzY0OC00MGY1LTg1MTUtYTdhMDNmYmYzNmQ12AIF4AIB&sid=0671f6e18fe455afd933f1aaccfd3eef&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.html%3Flabel%3Dgen173nr-1FCAEoggI46AdIM1gEaAaIAQGYATG4ARfIAQzYAQHoAQH4AQKIAgGoAgO4AvzJmYEGwAIB0gIkZDQ0YzIzZTItYzY0OC00MGY1LTg1MTUtYTdhMDNmYmYzNmQ12AIF4AIB%3Bsid%3D0671f6e18fe455afd933f1aaccfd3eef%3Bsb_price_type%3Dtotal%3Bsrpvid%3D44075001b3020021%26%3B&ss=Ankara%2C+Central+Anatolia+Region%2C+Turkey&is_ski_area=&checkin_year=&checkin_month=&checkout_year=&checkout_month=&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&ss_raw=Ankara&ac_position=0&ac_langcode=en&ac_click_type=b&dest_id=-735338&dest_type=city&iata=ANK&place_id_lat=39.91235&place_id_lon=32.851269&search_pageview_id=9f1c5002fc8700f7&search_selected=true&search_pageview_id=9f1c5002fc8700f7&ac_suggestion_list_length=5&ac_suggestion_theme_list_length=0';
 
 
 async function getdata(page){
@@ -19,7 +19,7 @@ async function getdata(page){
     return hotels;
 }
 
-(async () => {
+async function scrap() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(bookingUrl);
@@ -37,25 +37,28 @@ async function getdata(page){
     }
 
     //converting results to csv file
-    // const csvWriter = createCsvWriter({
-    //   path: 'file.csv',
-    //   header: [
-    //       {id: 'name', title: 'NAME'},
-    //       {id: 'city', title: 'CITY'},
-    //       {id: 'country', title: 'COUNTRY'}
-    //   ]
-    // });
-    // let res = [];
-    // results.forEach(element => {
-    //   let obj = new Object();
-    //   obj.name = element.split(',')[0];
-    //   obj.city = element.split(',')[1];
-    //   obj.country = element.split(',')[2];
-    //   res.push(obj);
-    // });
-    // csvWriter.writeRecords(res)
-    // .then(() => {
-    //   console.log('...Done');
-    // });
+
+    const csvWriter = createCsvWriter({
+      path: 'file.csv',
+      header: [
+          {id: 'name', title: 'NAME'},
+          {id: 'city', title: 'CITY'},
+          {id: 'country', title: 'COUNTRY'}
+      ]
+    });
+    let res = [];
+    results.forEach(element => {
+      let obj = new Object();
+      obj.name = element.split(',')[0];
+      obj.city = element.split(',')[1];
+      obj.country = element.split(',')[2];
+      res.push(obj);
+    });
+    csvWriter.writeRecords(res)
+    .then(() => {
+      console.log('...Done');
+    });
     await browser.close();
-})();
+};
+
+module.exports = scrap;
