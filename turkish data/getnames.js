eb041d5051704a3f11c95f390dc65b2ef70015fe
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-15 22:31:53 +0300

    split names

--- turkish data/getnames.js
+++ turkish data/getnames.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const url = "http://www.turkishculture.org/lifestyles/turkish-culture-portal/names-meaning-321.htm";
 
 async function scrap() {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto(url);
     let datas = await page.$$('div.page-body strong');
@@ -11,12 +11,13 @@ async function scrap() {
     for(data of datas){
         try{
             let emri = await data.$eval(('div.page-body strong > span'),node => node.innerText);
-            results.push(emri);
+            results.push(emri.split('-')[0]);
         }
         catch(err){
             console.log(err);
         }
     }
+    console.log(results);
     console.log( "Length of names is : " + results.length);
     await browser.close();
     return results;
