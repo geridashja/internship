d87d50d0613b80ed80e77a6b5562c9e1bf44996b
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-15 21:29:47 +0300

    firstnames

--- turkish data/getnames.js
+++ turkish data/getnames.js
@@ -6,13 +6,29 @@ async function scrap() {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto(url);
-    let results = [];
-    while( page.$eval(('#page-content > span > strong'),node => node.innerText)){
-        let name = await page.$eval(('#page-content > span > strong'),node => node.innerText);
-        results.push(name);
-    } 
-    await browser.close();
+    let datas = await page.$$('div.page-body strong');
+    let results= [];
+    var i =0;
+    for(data of datas){
+        try{
+            let emri = await data.$eval(('div.page-body strong > span'),node => node.innerText);
+            results.push(emri);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+    // let emri = await page.$eval(('#page-content > p > strong > span'),node => node.innerText);
+    // for(i < data1.length){
+    //     // let emri = await data.$$('#page-content > strong')[i++].innerText;
+    //     // results.push(emri);
+    //     let data = await page.$$('#page-content > p > strong > span').innerText;
+    //     console.log(data)
+    //     i++;
+    // }
     console.log(results);
+    console.log(results.length);
+    await browser.close();
     return results;
 };
 
