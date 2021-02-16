5946cc4db809297ba1994a21a32dc1800d964fbb
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-16 18:28:13 +0300

    saving firstnames

--- turkish data/app.js
+++ turkish data/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs')
 
- const app = express();
+
+const app = express();
 
 app.get('/',async(req,res) => {
     try {
-        // let lastnames = [];
-        // let firstnames = [];
         res.sendFile('home.html', { root: path.join(__dirname) });
     } catch (error) {
         console.log(error.message);
