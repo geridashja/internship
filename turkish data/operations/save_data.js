c23453a48221f6e3eac91acddb5154e58e9b5fcf
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-17 21:51:53 +0300

    other columns

--- turkish data/operations/save_data.js
+++ turkish data/operations/save_data.js
@@ -10,21 +10,25 @@ const csvFilePath2='./files/turkish_names.csv'
 async function savedata(){
     csv({
         noheader: false,
-        headers: ['Names','Lastnames']
+        headers: ['Names','Lastnames','Fathername','Mothername','Gender']
     })
     .fromFile(csvFilePath2)
         .then((jsonObj)=>{
             jsonObj.forEach(async element => {
                 let data = birthday_generator();
-                const birthday = data[0];
-                const year = data[1];
-                const month = data[2];
-                const day = data[3];
+                let birthday = data[0];
+                let year = data[1];
+                let month = data[2];
+                let day = data[3];
                 let firstname = element.Names.split(' ')[0];
                 let lastname = element.Lastnames.split(' ')[0];
+                let father = element.Fathername.split(' ')[0];
+                let mother = element.Mothername.split(' ')[0];
+                let gender = element.Gender.split(' ')[0];
                 let turk_iid = turk_id_gen();
-                // let newitem1 = await pool.query("INSERT INTO data (turk_id,firstname,lastname) VALUES ($1,$2,$3)", [turk_iid, firstname,lastname]);
+                let newitem1 = await pool.query("INSERT INTO data (turk_id,firstname,lastname,fathername,mothername,birthday,gender,birthyear,birthmonth,birthdayy) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)", [turk_iid, firstname,lastname,father,mother,birthday,gender,year,month,day]);
             })
+        console.log("FINISH");
     })
 }
 
