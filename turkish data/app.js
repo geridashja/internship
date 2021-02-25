9b10a0694927611d5a91b298c785e6a6f5c189c5
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-25 16:16:57 +0300

    updating colum fixed

--- turkish data/app.js
+++ turkish data/app.js
@@ -10,17 +10,6 @@ app.get('/',async(req,res) => {
     try {
         
         await saveaccom();
-        // var i =0;
-        // .then(async res => {
-        //     let hotel_iid = await pool.query('SELECT hotel_id FROM otel');
-        //     // pool.query(`UPDATE person SET age = "${}"  WHERE id = 3`, (err, res) => {
-        //     //     pool.end();
-        //     // });
-        //     hotel_iid.rows.slice(-(hotel_iid.rows.length)).forEach(async element => {
-        //         let hotel_id = hotel_iid.rows[i++].hotel_id;
-        //         console.log(hotel_id);
-        //     });
-        // })
     } catch (error) {
         console.log(error.message);
     }
--- turkish data/operations/accommodation.js
+++ turkish data/operations/accommodation.js
@@ -10,34 +10,37 @@ const csvFilePath2='./files/turkish_names.csv'
 async function saveaccom(){
     await savehotels();
     await savedata();
-    let hotel_iid = await pool.query('SELECT hotel_id FROM otel');
+    let arr = [];
+    let hotel_iid = await pool.query('SELECT hotel_id FROM otel').then(res =>{
+         res.rows.forEach(elem =>{
+            arr.push(elem.hotel_id)
+        });
+    });
+    console.log(arr.length);
+    let arr1 = [];
+    let turk_iid = await pool.query('SELECT turk_id FROM person').then(res =>{
+         res.rows.forEach(elem =>{
+            arr1.push(elem.turk_id)
+        });
+    });
+    console.log(arr1.length)
     //.slice(-(hotel_iid.rows.length))
     var i = 0;
     var j=0;
-    let len = hotel_iid.rows.length;
-    let turk_iid = await pool.query('SELECT turk_id FROM person');
-    for(k = 0;k< len;k++){
-        let hotel_id = hotel_iid.rows[j++].hotel_id;
+    // let len = hotel_iid.rows.length;
+    // let turk_iid = await pool.query('SELECT turk_id FROM person');
+    for(k = 0;k< arr1.length;k++){
+        // let hotel_id = hotel_iid.rows[j++].hotel_id;
         let room_num = random_room_num(1,1000);
         let chars_ofplate = plate_stringsgen();
         let first_nums_ofplate = random_room_num(1,81);
         let last_nums_ofplate = random_room_num(100,999);
         let full_plate = pad(first_nums_ofplate).toString() + " " + chars_ofplate + " " + last_nums_ofplate;
-        if(person_id = turk_iid.rows[i++].turk_id != undefined){
-            var person_id = turk_iid.rows[i++].turk_id;
-        }
-        else{
-            person_id = 1;
-        }
+        let hotel_id = arr[j++];
+        let person_id = arr1[i++];
         let newitem1 = await pool.query("INSERT INTO accommodation (hotel_id,person_id,room_number,turkish_plate) VALUES ($1,$2,$3,$4)", [hotel_id,person_id,room_num,full_plate]);
     }
-    // let turk_iid = await pool.query('SELECT turk_id FROM person');
-    // for(v = 0;v< turk_iid.rows.length;v++){
-    //     let person_id = turk_iid.rows[v++].turk_id;
-    //     let newitem1 = await pool.query(`UPDATE accommodation SET person_id = $1`, [1]);
-    //     console.log(person_id)
-    // }
-    
+    // pool.query("UPDATE table2 t2 SET val2 = t1.val1 FROM   table1 t1 WHERE  t2.table2_id = t1.table2_id")
 }
 
 //converting 1,2,3 etc to 01,02,03 etc
