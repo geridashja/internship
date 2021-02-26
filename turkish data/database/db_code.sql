14209c2bdde604e167f950d218aa260b2b42645d
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-26 12:44:23 +0300

    db oper

---
+++ turkish data/database/db_code.sql
@@ -0,0 +1,52 @@
+
+CREATE TABLE person(
+turk_id  BIGINT,
+firstname VARCHAR(100),
+lastname VARCHAR(100),
+fathername VARCHAR(100),
+mothername VARCHAR(100),
+gender VARCHAR(100),
+ismarried VARCHAR(100),
+birthyear INT,
+birthmonth INT,
+birthdayy INT,
+data DATE,
+birthcountry VARCHAR(100),
+otel_id BIGINT REFERENCES otel (hotel_id)
+);
+
+
+CREATE TABLE otel(
+hotel_id INT PRIMARY KEY,
+hotel_name VARCHAR(100),
+city VARCHAR(100),
+country VARCHAR(100),
+accommodation_id BIGINT REFERENCES accommodation(hotel_id)
+);
+
+
+CREATE TABLE accommodation(
+hotel_id  INT PRIMARY KEY,
+person_id BIGINT,
+room_number INT,
+turkish_plate VARCHAR(100)
+);
+
+
+
+UPDATE otel o
+SET accommodation_id = d.hotel_id
+FROM accommodation d
+WHERE d.hotel_id = o.hotel_id;
+
+
+UPDATE accommodation o
+SET person_id = d.turk_id
+FROM person d
+WHERE d.otel_id = o.hotel_id;
+
+
+SELECT * FROM person INNER JOIN otel ON person.otel_id = otel.hotel_id INNER JOIN accommodation ON otel.accommodation_id = accommodation.hotel_id;
+
+
+\copy (SELECT * FROM person INNER JOIN otel ON person.otel_id = otel.hotel_id INNER JOIN accommodation ON otel.accommodation_id = accommodation.hotel_id) TO 'C:\Users\shqip\Desktop\dump.csv' DELIMITER ',' CSV HEADER;
