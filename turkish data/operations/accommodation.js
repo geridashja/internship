b2f95614fbdb28da39ab28428c150f76fea7ab6d
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-02-26 12:39:47 +0300

    table dump

--- turkish data/operations/accommodation.js
+++ turkish data/operations/accommodation.js
@@ -4,7 +4,7 @@ const savehotels = require('./hotles');
 const e = require('express');
 
 
-const csvFilePath2='./files/turkish_names.csv'
+const csvFilePath2 = './files/turkish_names.csv'
 
 
 async function saveaccom(){
@@ -12,38 +12,29 @@ async function saveaccom(){
     await savedata();
     let arr = [];
     let hotel_iid = await pool.query('SELECT hotel_id FROM otel').then(res =>{
-         res.rows.forEach(elem =>{
+        res.rows.forEach(elem =>{
             arr.push(elem.hotel_id)
         });
     });
-    // console.log(arr.length);
-    // let arr1 = [];
-    // .then(res =>{
-    //      res.rows.forEach(elem =>{
-    //          console.log(elem.turk_id)
-    //         arr1.push(elem.turk_id)
-    //     });
-    // });
-    // console.log(arr1.length)
-    //.slice(-(hotel_iid.rows.length))
-    var i = 0;
     var j=0;
-    // let len = hotel_iid.rows.length;
-    // let turk_iid = await pool.query('SELECT turk_id FROM person');
+
     for(k = 0;k< arr.length;k++){
-        // let hotel_id = hotel_iid.rows[j++].hotel_id;
+
         let room_num = random_room_num(1,1000);
+
         let chars_ofplate = plate_stringsgen();
+
         let first_nums_ofplate = random_room_num(1,81);
         let last_nums_ofplate = random_room_num(100,999);
         let full_plate = pad(first_nums_ofplate).toString() + " " + chars_ofplate + " " + last_nums_ofplate;
+
         let hotel_id = arr[j++];
+
         let newitem1 = await pool.query("INSERT INTO accommodation (hotel_id,room_number,turkish_plate) VALUES ($1,$2,$3)", [hotel_id,room_num,full_plate]);
     }
-    // pool.query("UPDATE table2 t2 SET val2 = t1.val1 FROM   table1 t1 WHERE  t2.table2_id = t1.table2_id")
 }
 
-//converting 1,2,3 etc to 01,02,03 etc
+
 function pad(num) {
     if(num < 10){
         num = num.toString();
@@ -51,12 +42,12 @@ function pad(num) {
     }
     return num;
 }
-//rand nums
+
 function random_room_num(min, max) {
     return Math.floor(min + Math.random()*(max + 1 - min))
 }
 
-//rand chars
+
 function plate_stringsgen(){
    let length = 2;
    var result = '';
--- turkish data/operations/save_data.js
+++ turkish data/operations/save_data.js
@@ -15,38 +15,41 @@ async function savedata(){
     .fromFile(csvFilePath2)
         .then((jsonObj)=>{
             jsonObj.slice(-(hotel_iid.rows.length)).forEach(async element => {
-                //getting birthday details
+
                 let data = birthday_generator();
                 let birthday = data[0];
                 let year = data[1];
                 let month = data[2];
                 let day = data[3];
-                //getting the age of person
+
                 var d = new Date();
+
                 var currentyear = d.getFullYear();
                 let age = currentyear-year;
+
                 var isMarried = "";
                 if(age > 18)
                     isMarried = "Yes"
                 else
                     isMarried = "No"
-                //details from csv
+
                 let firstname = element.Names.split(' ')[0];
                 let lastname = element.Lastnames.split(' ')[0];
                 let father = element.Fathername.split(' ')[0];
                 let mother = element.Mothername.split(' ')[0];
                 let gender = element.Gender.split(' ')[0];
-                //getting the turkish id generated
+
+
                 let turk_iid = turk_id_gen();
+
                 let birthobj = new Date(birthday);
-                //country
+
+
                 let birthcountry = "TURKEY";
-                // let cities = ["Adana","Adiyaman","Afyon","Agri","Aksaray","Amasya","Ankara","Antalya","Ardahan","Artvin","Aydin","Balikesir","Bartin","Batman","Bayburt","Bilecik","Bingol","Bitlis","Bolu","Burdur","Bursa","Canakkale","Cankiri","Corum","Denizli","Diyarbakir","Duzce","Edirne","Elazig","Erzincan","Erzurum","Eskisehir","Gaziantep","Giresun","Gumushane","Hakkari","Hatay","Igdir","Isparta","Istanbul","Izmir","Kahramanmaras","Karabuk","Karaman","Kars","Kastamonu","Kayseri","Kilis","Kirikkale","Kirklareli","Kirsehir","Kocaeli","Konya","Kutahya","Malatya","Manisa","Mardin","Mersin","Mugla","Mus","Nevsehir","Nigde","Ordu","Osmaniye","Rize","Sakarya","Samsun","Sanliurfa","Siirt","Sinop","Sirnak","Sivas","Tekirdag","Tokat","Trabzon","Tunceli","Usak","Van","Yalova","Yozgat","Zonguldak"];
-                //randomly select a city
-                // let randomIndex = Math.floor(Math.random() * cities.length); 
-                // let randomcity = cities[randomIndex];
+
                 let hotel_id = hotel_iid.rows[i++].hotel_id
-                //saving to database
+
+
                 let newitem1 = await pool.query("INSERT INTO person (turk_id,firstname,lastname,fathername,mothername,gender,ismarried,birthyear,birthmonth,birthdayy,data,birthcountry,otel_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)", [turk_iid, firstname,lastname,father,mother,gender,isMarried,year,month,day,birthobj,birthcountry,hotel_id]);
             })
     })
